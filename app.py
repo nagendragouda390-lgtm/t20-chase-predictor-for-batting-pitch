9ca0866353ecf177f82e8f3789dcb3de627641cf
Nagendra Gouda <nagendragouda390@gmail.com>
2026-08-09 13:09:11 +0530

    Refactor model file paths in app.py
    Updated model file paths to include version suffixes and added new model paths.

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ from flask import Flask, render_template, request, redirect, url_for, g
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(APP_DIR, "data.db")
-MODEL_PATH = os.path.join(APP_DIR, "model", "model_lr.pkl")
-Model_200 = os.path.join(APP_DIR, "model","model_200+.pkl")
-MODEL_175 = os.path.join(APP_DIR, "model","model_175+.pkl")
+MODEL_175 = os.path.join(APP_DIR, "model", "model_175_+.pkl")
+MODEL_200 = os.path.join(APP_DIR, "model","model_200_+.pkl")
+MODEL_150 = os.path.join(APP_DIR, "model","model_150_+.pkl")
+MODEL_120 = os.path.join(APP_DIR, "model","model_120_+.pkl")
+MODEL_LOW = os.path.join(APP_DIR, "model","model_low.pkl")
 TEAMS_PATH = os.path.join(APP_DIR, "model", "teams.pkl")
 
 app = Flask(__name__)
